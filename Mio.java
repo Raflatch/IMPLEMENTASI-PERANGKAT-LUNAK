0cde383247104e82ed84b2e07495e347c4ed3877
Raflatch <123710333+Raflatch@users.noreply.github.com>
2024-10-31 00:43:07 +0700

    Create Mio.java

---
+++ Mio.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.tugas_class_diagram;
+
+/**
+ *
+ * @author salbanirafael
+ */
+public class Mio extends AutoCarRPL {
+    public Mio() {
+        System.out.println("Ini adalah Mio (Motor dengan roda 2).");
+        pasangRoda(2);  // Pasang roda sebanyak 2
+    }
+}
