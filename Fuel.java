86290f9dcdf8b0523a5a994ee989f0e3898582cc
Raflatch <123710333+Raflatch@users.noreply.github.com>
2024-10-31 00:37:57 +0700

    Create Fuel.java

---
+++ Fuel.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.tugas_class_diagram;
+
+/**
+ *
+ * @author salbanirafael
+public class Fuel {
+    private String jenis;
+
+    public Fuel(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+}
