3398170acbdb3d2a1aef768ac47f264fc2b7fd98
Raflatch <123710333+Raflatch@users.noreply.github.com>
2024-10-31 00:44:29 +0700

    Create Roda.java

---
+++ Roda.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.tugas_class_diagram;
+
+/**
+ *
+ * @author salbanirafael
+ */
+public class Roda {
+    public Roda() {
+        System.out.println("Satu roda dipasang.");
+    }
+}
