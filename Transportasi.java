9f41d176ac7eb72afbc2df4ea025db8125d74d10
Raflatch <123710333+Raflatch@users.noreply.github.com>
2024-10-31 00:47:33 +0700

    Create Transportasi.java

---
+++ Transportasi.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.tugas_class_diagram;
+
+/**
+ *
+ * @author salbanirafael
+ */
+
+public interface Transportasi {
+    void setBahanBakar(String bahanBakar);
+    void setKecepatan(int kecepatan);
+}
+
